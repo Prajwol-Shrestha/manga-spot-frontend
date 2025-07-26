67e22d251acae0d9b93927bb73ffcc420284f8fd
Prajwol-Shrestha <prajwolshrestha08@gmail.com>
2025-07-26 22:58:14 +0545

    fix: bookmarks

--- src/services/bookmarksService.ts
+++ src/services/bookmarksService.ts
@@ -4,8 +4,14 @@ import { IBookmarkResponse } from '@/types/bookmark';
 import { IQueryParams } from '@/types/common';
 
 export async function getAllBookmarks(queryParams?: IQueryParams) {
+  const config: RequestInit = {
+    next: {
+      tags: ['bookmarks'],
+    },
+  };
   const result = await universalFetcher<IBookmarkResponse>(END_POINTS.bookmarks.getBookmarks, {
     queryParams,
+    config,
   });
   return result;
 }
