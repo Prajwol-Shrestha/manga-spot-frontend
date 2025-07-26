e8172e9316c99ed25c9646ab4e923776be4a27cb
Prajwol-Shrestha <prajwolshrestha08@gmail.com>
2025-07-26 23:42:51 +0545

    fix: middleware authroutes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,17 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const protectedRoutes = ['/profile', '/bookmarks', '/history'];
 
+const authRoutes = ['/login', '/signup'];
+
 export default function middleware(request: NextRequest) {
   const token = request.cookies.get('accessToken');
 
   if (!token && protectedRoutes.includes(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  if (token && authRoutes.includes(request.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL('/', request.url));
   }
 
   return NextResponse.next();
