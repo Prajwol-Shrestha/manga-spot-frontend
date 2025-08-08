6eaac592a9e2fde486718629e51fd5bd4b742d9d
Prajwol-Shrestha <prajwolshrestha08@gmail.com>
2025-08-08 18:26:04 +0545

    fix: cookie domain cookie issue

--- src/lib/fetcher.client.ts
+++ src/lib/fetcher.client.ts
@@ -1,10 +1,12 @@
 import { baseURL, IFetcherConfig } from './fetcher';
 import buildQueryParams from './buildQueryParams';
 import { logout } from '@/helpers/logout';
+import { getAccessTokenFromCookie } from './cookies';
 
 export async function clientFetcher<T = unknown>(url: string, options?: IFetcherConfig): Promise<T> {
   const { queryParams, config } = options || {};
   const endpoint = new URL(`${baseURL}${url}`);
+  const accessToken = await getAccessTokenFromCookie();
 
   if (queryParams) {
     const params = buildQueryParams(queryParams);
@@ -13,9 +15,10 @@ export async function clientFetcher<T = unknown>(url: string, options?: IFetcher
 
   const response = await fetch(endpoint.toString(), {
     cache: 'force-cache',
-    credentials: 'include',
+    // credentials: 'include', // since FE is setting the cookie.. then it will only send the cookie to the same domain.. and BE is on different domain right now
     headers: {
       'Content-Type': 'application/json',
+      ...(accessToken && { Authorization: `Bearer ${accessToken}` }),
     },
     ...config,
   });
